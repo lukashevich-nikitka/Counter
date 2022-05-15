d6429891618f0eda05d4b36d02607f1a351db176
Nikita <lukashevich-nikitka@bk.ru>
2022-05-15 18:05:51 +0300

    feat: counter done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Counter />
+    )
+  }
+}
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 0}
+  }
+
+  render() {
+    return (
+      <>
+      <div className='app'>
+        <Screen value = {this.state.value}/>
+        <div className='buttons-area'>
+          <button className='button' onClick={() => this.setState({ value: this.state.value + 1 })}>+</button>
+          <button className='button' onClick={() => this.setState({ value: this.state.value - 1 })}>-</button>
+        </div>
+      </div>
+      </>
+    )
+  }
+}
+
+class Screen extends React.Component {
+  render() {
+    return (
+      <div className='screen'>{this.props.value}</div>
+    )
+  }
 }
 
 export default App;
