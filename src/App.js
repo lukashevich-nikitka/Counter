a76810b747e118097b55bf343fa02b335569a908
Nikita <lukashevich-nikitka@bk.ru>
2022-05-16 22:09:14 +0300

    feat: rewrite app on functional components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Counter />
-    )
-  }
+
+function App() {
+  return <> 
+  <Counter />
+  </>
 }
 
-class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 0}
-  }
-
-  render() {
-    return (
-      <>
-      <div className='app'>
-        <Screen value = {this.state.value}/>
-        <div className='buttons-area'>
-          <button className='button' onClick={() => this.setState({ value: this.state.value + 1 })}>+</button>
-          <button className='button' onClick={() => this.setState({ value: this.state.value - 1 })}>-</button>
-        </div>
+function Counter() {
+  const [state, setState] = useState(0)
+  return (
+    <>
+    <div className='app'>
+      <Screen value = {state}/>
+      <div className='buttons-area'>
+        <button className='button' onClick={() => setState(state + 1 )}>+</button>
+        <button className='button' onClick={() => setState(state - 1)}>-</button>
       </div>
-      </>
-    )
-  }
+    </div>
+    </>
+  )
 }
 
-class Screen extends React.Component {
-  render() {
-    return (
-      <div className='screen'>{this.props.value}</div>
-    )
-  }
+function Screen(props) {
+  return <div className='screen'>{props.value}</div>
 }
 
 export default App;
